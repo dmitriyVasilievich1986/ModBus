021cef9c97c98160f5a0ad311ccb4d86e2aee383
Dmitriy Shcherbachenya <dmitriyvasil@gmail.com>
2020-01-27 09:09:32 +0300

    Программа для обмена по modbus

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Modbus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Transmit = new System.Windows.Forms.Button();
+            this.COMPort = new System.Windows.Forms.ComboBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Transmit
+            // 
+            this.Transmit.Location = new System.Drawing.Point(139, 155);
+            this.Transmit.Name = "Transmit";
+            this.Transmit.Size = new System.Drawing.Size(75, 23);
+            this.Transmit.TabIndex = 0;
+            this.Transmit.Text = "Transmit";
+            this.Transmit.UseVisualStyleBackColor = true;
+            this.Transmit.Click += new System.EventHandler(this.Transmit_Click);
+            // 
+            // COMPort
+            // 
+            this.COMPort.FormattingEnabled = true;
+            this.COMPort.Location = new System.Drawing.Point(28, 22);
+            this.COMPort.Name = "COMPort";
+            this.COMPort.Size = new System.Drawing.Size(121, 21);
+            this.COMPort.TabIndex = 9;
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(180, 22);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 10;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(283, 22);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 11;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(385, 22);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(75, 23);
+            this.Refresh.TabIndex = 12;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(432, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 231);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(460, 207);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(28, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Text = "115200";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.COMPort);
+            this.Controls.Add(this.Transmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Transmit;
+        private System.Windows.Forms.ComboBox COMPort;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModBus_Library;
+
+namespace Modbus
+{
+    public partial class Form1 : Form
+    {
+        ModBus_Libra PortA = new ModBus_Libra(new SerialPort());
+        List<string> Data = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            COMPort.Items.AddRange(SerialPort.GetPortNames());
+            COMPort.Text = Properties.Settings.Default.ComPort;
+            textBox3.Text = Properties.Settings.Default.Speed.ToString();
+            Close.Enabled = false;
+            Open.Enabled = true;
+
+            textBox1.KeyDown += (s, e) => { if (e.KeyCode == Keys.Enter) Transmit_Click(null, null); };
+            Refresh.Click += (s, e) => { COMPort.Items.Clear(); COMPort.Items.AddRange(SerialPort.GetPortNames()); };
+            Close.Click += (s, e) => { PortA.Close(); Open.Enabled = true; Close.Enabled = false; };
+            COMPort.TextChanged += (s, e) => { Properties.Settings.Default.ComPort = COMPort.Text;Properties.Settings.Default.Save(); };
+            textBox3.TextChanged += (s, e) =>
+            {   
+                try
+                {
+                    int a = Convert.ToInt32(textBox3.Text);
+                    Properties.Settings.Default.Speed = a;
+                    Properties.Settings.Default.Save();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            };
+            Open.Click += (s, e) =>
+            {
+                try
+                {
+                    PortA.Name = COMPort.Text;
+                    PortA.Speed = Convert.ToInt32(textBox3.Text);
+                    PortA.Open();
+                    Open.Enabled = false; Close.Enabled = true;
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            };
+
+            PortA.Receive_Event +=()=> BeginInvoke((MethodInvoker)(() =>
+            {
+                Data.Add(PortA.Receive_Array);
+                textBox2.Lines = Data.ToArray();
+                textBox2.SelectionStart = textBox2.TextLength;
+                textBox2.ScrollToCaret();
+                while (Data.Count > 100) Data.RemoveAt(0);
+                label1.Text = PortA.Result[0].ToString();
+            }));
+            PortA.Transmit_Event += () => BeginInvoke((MethodInvoker)(() =>
+            {
+                Data.Add(PortA.Transmit_Array);
+                textBox2.Lines = Data.ToArray();
+                textBox2.SelectionStart = textBox2.TextLength;
+                textBox2.ScrollToCaret();
+                while (Data.Count > 100) Data.RemoveAt(0);
+            }));
+        }
+
+        private void Transmit_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0) return;
+            
+            string asa = textBox1.Text.Replace(" ", string.Empty);
+            byte[] send = new byte[asa.Length / 2];
+            for (int a = 0; a < asa.Length; a += 2)
+            {
+                try
+                {
+                    send[a / 2]=Convert.ToByte(asa.Substring(a, 2), 16);
+
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            PortA.Transmit(send);
+        }
+    }
+}
